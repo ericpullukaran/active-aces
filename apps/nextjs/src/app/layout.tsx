1e61741e8b1ae75b16f13daf1660183dc09eecdf
Eric Paul <ericcpaul00@gmail.com>
2024-03-22 08:59:40 +1100

    started process of linking ui libaries tgt

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,35 +1,35 @@
 import type { Metadata, Viewport } from "next";
+import { ThemeProvider, ThemeToggle } from "@/components/theme";
+import { Button } from "@/components/ui/button";
 import { ClerkProvider } from "@clerk/nextjs";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
-
-import { cn } from "@acme/ui";
-import { ThemeProvider, ThemeToggle } from "@acme/ui/theme";
-import { Toaster } from "@acme/ui/toast";
 
 import { env } from "~/env";
 import { TRPCReactProvider } from "~/trpc/react";
 
 import "~/app/globals.css";
 
+import { cn } from "~/utils/cn";
+
 export const metadata: Metadata = {
   metadataBase: new URL(
     env.VERCEL_ENV === "production"
-      ? "https://turbo.t3.gg"
+      ? "https://activeaces.com"
       : "http://localhost:3000",
   ),
-  title: "Create T3 Turbo",
-  description: "Simple monorepo with shared backend for web & mobile apps",
+  title: "Active Aces",
+  description: "A fitness and gym tracking platform",
   openGraph: {
-    title: "Create T3 Turbo",
-    description: "Simple monorepo with shared backend for web & mobile apps",
-    url: "https://create-t3-turbo.vercel.app",
-    siteName: "Create T3 Turbo",
+    title: "Active Aces",
+    description: "A fitness and gym tracking platform",
+    url: "https://activeaces.com",
+    siteName: "Active Aces",
   },
   twitter: {
     card: "summary_large_image",
-    site: "@jullerino",
-    creator: "@jullerino",
+    site: "@activeaces",
+    creator: "@activeaces",
   },
 };
 
@@ -50,13 +50,10 @@ export default function RootLayout(props: { children: React.ReactNode }) {
           GeistMono.variable,
         )}
       >
+        <Button>Hey</Button>
         <ClerkProvider publishableKey={env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
-          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+          <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
             <TRPCReactProvider>{props.children}</TRPCReactProvider>
-            <div className="absolute bottom-4 right-4">
-              <ThemeToggle />
-            </div>
-            <Toaster />
           </ThemeProvider>
         </ClerkProvider>
       </body>
---
+++ apps/nextjs/src/components/theme.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import * as React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { ThemeProvider, useTheme } from "next-themes";
+
+function ThemeToggle() {
+  const { theme, setTheme } = useTheme();
+  const [position, setPosition] = React.useState("dark");
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline">
+          {theme === "light" && (
+            <SunIcon className="size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          )}
+          {theme === "dark" && (
+            <MoonIcon className="absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          )}
+        </Button>
+      </DropdownMenuTrigger>
+
+      <DropdownMenuContent className="w-56">
+        <DropdownMenuRadioGroup value={position} onValueChange={setPosition}>
+          <DropdownMenuRadioItem
+            onClick={() => setTheme("light")}
+            value="light"
+          >
+            <SunIcon className="size-5 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem onClick={() => setTheme("dark")} value="dark">
+            <MoonIcon className="absolute size-5 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          </DropdownMenuRadioItem>
+          <DropdownMenuRadioItem
+            onClick={() => setTheme("system")}
+            value="system"
+          >
+            System
+          </DropdownMenuRadioItem>
+        </DropdownMenuRadioGroup>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export { ThemeProvider, ThemeToggle };
