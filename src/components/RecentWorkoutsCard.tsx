b5b8cfd9952b81546781c7f731647a0861966e0d
Eric Paul <ericcpaul00@gmail.com>
2025-04-25 10:22:51 +1000

    chore: clean up code

--- src/components/RecentWorkoutsCard.tsx
+++ src/components/RecentWorkoutsCard.tsx
@@ -13,28 +13,36 @@ export default function RecentWorkoutsCard() {
   const trpc = useTRPC()
   const workoutHistory = useQuery(trpc.workouts.history.queryOptions())
 
-  if (!workoutHistory.data) {
-    return <div className="bg-card h-96 animate-pulse rounded-lg"></div>
-  }
-
-  if (workoutHistory.data.length) {
+  if (workoutHistory.isError) {
     return (
-      <div className="bg-card rounded-lg p-4">
-        <div className="mb-3 flex items-center">
-          <h3 className="flex-1 text-lg font-semibold">Recent Workouts</h3>
-          <Button variant={"ghost"} className="bg-card" asChild>
-            <Link href={"/dashboard/history"} className="flex text-right">
-              See More
-              <ArrowRight className="ml-1" />
-            </Link>
-          </Button>
-        </div>
-        <div className="flex flex-col gap-3">
-          {workoutHistory.data?.map((w) => (
-            <WorkoutHistoryCard key={w.id} workout={w} internalNav={true} />
-          ))}
+      <div className="bg-card h-96 animate-pulse rounded-lg">
+        <div className="flex h-full flex-col justify-center">
+          <p className="text-muted-foreground">Error loading workouts</p>
         </div>
       </div>
     )
   }
+
+  if (workoutHistory.isLoading) {
+    return <div className="bg-card h-96 animate-pulse rounded-lg"></div>
+  }
+
+  return (
+    <div className="bg-card rounded-lg p-4">
+      <div className="mb-3 flex items-center">
+        <h3 className="flex-1 text-lg font-semibold">Recent Workouts</h3>
+        <Button variant={"ghost"} className="bg-card" asChild>
+          <Link href={"/dashboard/history"} className="flex text-right">
+            See More
+            <ArrowRight className="ml-1" />
+          </Link>
+        </Button>
+      </div>
+      <div className="flex flex-col gap-3">
+        {workoutHistory.data?.map((w) => (
+          <WorkoutHistoryCard key={w.id} workout={w} internalNav={true} />
+        ))}
+      </div>
+    </div>
+  )
 }
